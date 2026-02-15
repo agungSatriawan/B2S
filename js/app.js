04eba9d9863db152f0a53050fdd924bf2be728a0
Agung <45866024+agungSatriawan@users.noreply.github.com>
2026-02-15 22:34:41 +0700

    v3

--- js/app.js
+++ js/app.js
@@ -88,6 +88,76 @@ const mapping = {
     "Target_RFI_Tsel": "Target_RFI_Tsel",
     "PM": "PM"
 };
+
+const statusMapping = {
+    "01. On Air": "01. On Air",
+    "02. BAST CME DONE": "02. BAST CME DONE",
+    "03. BAST SITAC DONE": "03. BAST SITAC DONE",
+    "04. BAST SACME NY": "04. BAST SACME NY",
+    "05. BAST SACME DONE": "05. BAST SACME DONE",
+    "06. ATP DONE": "06. ATP",
+    "07. RFI": "07. RFI",
+    "08. SOFT RFI": "08. SOFT RFI",
+
+    "09. CME": "09. CME",
+    "09.a PLN DONE": "09. CME",
+    "09. b ME DONE": "09. CME",
+    "09.c ME OG": "09. CME",
+    "09.d ERECTION DONE": "09. CME",
+    "09.e ERECTION OG": "09. CME",
+    "09.f CURRING DONE": "09. CME",
+    "09.g CURRING OG": "09. CME",
+    "09.h POURING DONE": "09. CME",
+    "09.i POURING OG": "09. CME",
+    "09.j REBBARING DONE": "09. CME",
+    "09.k REBBARING OG": "09. CME",
+    "09.l EXCAVATION DONE": "09. CME",
+    "09.m EXCAVATION OG": "09. CME",
+    "09.n Site Opening/Clearing": "09. CME",
+
+    "10. RFC": "10. RFC",
+    "10.a IMB DONE": "10. RFC",
+    "10.b IMB OG": "11. SITAC 2",
+
+    "11. SITAC 2": "11. SITAC 2",
+    "11.a RFC DONE": "11. SITAC 2",
+    "11.b APD DONE": "11. SITAC 2",
+    "11.c APD OG": "11. SITAC 2",
+    "11.d SOIL/HAMMER DONE": "11. SITAC 2",
+    "11.e SOIL/HAMMER OG": "11. SITAC 2",
+    "11.f REKOM CAMAT DONE": "11. SITAC 2",
+    "11.g REKOM CAMAT OG": "11. SITAC 2",
+    "11. h REKOM LURAH DONE": "11. SITAC 2",
+    "11. I REKOM LURAH OG": "11. SITAC 2",
+    "11. j IW DONE": "11. SITAC 2",
+
+    "11.k IW OG": "12. SITAC 1",
+    "11. l BAN BAK DONE": "12. SITAC 1",
+    "11.m BAN BAK OG": "12. SITAC 1",
+
+    "12. VALIDASI DONE": "13. SIS",
+    "12.VALIDASI OG": "13. SIS",
+    "12. TSSR DONE": "13. SIS",
+    "13. TSSR OG": "13. SIS",
+    "14. REHUNTING": "13. SIS",
+    "15. HUNTING": "13. SIS",
+
+    "16. HOLD": "13. HOLD",
+    "17. DROP": "14. DROP",
+    "18. PKS OG": "15. PKS",
+    "19. PKS DONE": "15. PKS"
+};
+document.getElementById("Detail_Status_SACME")
+    .addEventListener("change", function () {
+
+        const selectedValue = this.value;
+
+        const generalStatus = statusMapping[selectedValue] || "";
+
+        document.getElementById("General_Status_SACME").value = generalStatus;
+
+    });
+
 async function loadData() {
     const res = await fetch(WEB_APP_URL);
     const data = await res.json();
@@ -107,13 +177,13 @@ function renderTable() {
       <td>${row.PID}</td>
       <td>${row.Site_ID_Tsel}</td>
       <td>${row.Site_Name_Tenant}</td>
-      <td>${row['Tinggi Tower']}</td>
-      <td>${row['Kandidat Terpilih (P/Q/R/Candidate Reject)']}</td>
+      <td class="text-center">${row['Tinggi Tower']}</td>
+      <td class="text-center">${row['Kandidat Terpilih (P/Q/R/Candidate Reject)']}</td>
       <td>${row['Lat_(NEW)']}</td>
       <td>${row['Long_(NEW)']}</td>
-      <td>${row.General_Status_SACME}</td>
-      <td>${row.Detail_Status_SACME}</td>
-      <td>${row.Remarks_Daily}</td>
+      <td class="text-nowrap">${row.General_Status_SACME}</td>
+      <td class="text-nowrap">${row.Detail_Status_SACME}</td>
+      <td class="text-truncate d-inline-blocks" style="max-width:70px;">${row.Remarks_Daily}</td>
     `;
 
         tr.addEventListener("click", () => openModal(row));
@@ -126,24 +196,47 @@ loadData();
 
 
 function openModal(data) {
-  
+
     document.getElementById("site_name").innerHTML = data.Site_Name_Tenant;
+
     Object.keys(mapping).forEach(function (key) {
 
         const elementId = mapping[key];
+        if (!elementId) return;
 
-        if (elementId && document.getElementById(elementId)) {
-            document.getElementById(elementId).value = data[key] ?? "";
+        const value = data[key] ?? "";
+
+        // ===== FIELD YANG PAKE TINYMCE =====
+        if (elementId === "Remarks_Daily" || elementId === "Alamat_NEW") {
+
+            let cleanText = cleanHTMLToText(value);
+            let formatted = cleanText.replace(/\n/g, "<br>");
+
+            tinymce.get(elementId)?.setContent(formatted);
+
+        } else {
+
+            const el = document.getElementById(elementId);
+            if (el) el.value = value;
+
         }
+
+        const detail = data.Detail_Status_SACME ?? "";
+        document.getElementById("Detail_Status_SACME").value = detail;
+
+        document.getElementById("General_Status_SACME").value =
+            statusMapping[detail] || "";
+
 
     });
 
-        const modal = new bootstrap.Modal(
-            document.getElementById("editModal")
-        );
-        modal.show();
+    const modal = new bootstrap.Modal(
+        document.getElementById("editModal")
+    );
 
+    modal.show();
 }
+
 
 
 let selectedRow = null;
@@ -156,27 +249,39 @@ document.getElementById("btnSave").addEventListener("click", async function () {
         const elementId = mapping[key];
         const el = document.getElementById(elementId);
 
-        if (el) {
+        if (elementId === "Remarks_Daily") {
+            updatedData[key] = tinymce
+                .get("Remarks_Daily")
+                .getContent({ format: "text" });
+        } else if (elementId === "Alamat_NEW"){
+            updatedData[key] = tinymce
+                .get("Alamat_NEW")
+                .getContent({ format: "text" });
+        } else if (el) {
             updatedData[key] = el.value;
         }
+
+        
+
     });
+
+    const formData = new URLSearchParams();
+    formData.append("data", JSON.stringify(updatedData));
 
     try {
 
-        const formData = new URLSearchParams();
-        formData.append("data", JSON.stringify(updatedData));
-
-        const response = await fetch(WEB_APP_URL, {
+        await fetch(WEB_APP_URL, {
             method: "POST",
-            body: formData
+            body: JSON.stringify(updatedData),
+            headers: {
+                "Content-Type": "text/plain"
+            },
+            mode: "no-cors"
         });
 
-        const result = await response.json();
 
-        if (result.status === "success") {
-            alert("✅ Data berhasil diupdate!");
-            loadData();
-        }
+        alert("✅ Data berhasil dikirim!");
+        loadData(); // reload table
 
     } catch (error) {
         console.error(error);
@@ -184,6 +289,31 @@ document.getElementById("btnSave").addEventListener("click", async function () {
     }
 
 });
+
+tinymce.init({
+    selector: 'textarea',
+    height: 300,
+    menubar: false,
+    plugins: 'lists link table code',
+    toolbar: 'undo redo | bold italic underline | bullist numlist | link | code',
+    branding: false,
+    forced_root_block: false,   // penting!
+    force_br_newlines: true,
+    force_p_newlines: false
+});
+
+function cleanHTMLToText(html) {
+    if (!html) return "";
+
+    return html
+        .replace(/<\/p>/gi, '\n')
+        .replace(/<br\s*\/?>/gi, '\n')
+        .replace(/<[^>]+>/g, '')
+        .trim();
+}
+
+
+
 
 
 
